650c4c58931af41145e4f6ab89aec267c0c1f69c
Dominick Smith <mail@dominickcs.com>
2026-06-23 13:47:04 -0500

    shikiConfig: updated theme to catppuccin-frappe

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.dominickcs.com',
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-frappe',
+      wrap: true,
+    }
+  }
 });
