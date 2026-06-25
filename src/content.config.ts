00ae3647754faa7c5d18cfbc89879e1d851ec5ac
Dominick Smith <mail@dominickcs.com>
2026-06-25 12:14:23 -0500

    Revision: removed description from blog post schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,7 +7,6 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      description: z.string(),
       publishDate: z.coerce.date(),
       updatedDate: z.coerce.date().optional(),
       heroImage: z.optional(image()),
